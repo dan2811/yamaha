bbce49cabca86ad5e1354316295220fedb99b1aa
Maria <ev.maria.pro@gmail.com>
2024-09-04 15:41:39 -0400

    add updates from the other branch

--- src/app/(admin)/admin/settings/rooms/page.tsx
+++ src/app/(admin)/admin/settings/rooms/page.tsx
@@ -6,7 +6,12 @@ import Link from "next/link";
 import { api } from "~/trpc/react";
 
 const Rooms = () => {
+  const { data: rooms, isLoading } = api.rooms.list.useQuery();
   const router = useRouter();
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  
   return (
     <div className="flex h-full flex-col p-2">
       <div className="flex-1">
@@ -15,6 +20,31 @@ const Rooms = () => {
           <Link href="/admin/settings/rooms/create">Add a room</Link>
         </AdminButton>
       </div>
+      <div>
+        <table className="w-full table-auto divide-y divide-gray-200">
+          <thead className="text-left">
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {rooms?.map(({id, name, description}) => {
+              return (
+                <tr
+                  title={"roomsList"}
+                  key={id}
+                  className="cursor-pointer border-b-2 border-purple-500/20 hover:bg-purple-500/40"
+                >
+                  <td>{name}</td>
+                  <td>{description}</td>
+                </tr>
+              );
+            },
+            )}
+          </tbody>
+        </table>
+      </div>  
     </div>
   );
 };
