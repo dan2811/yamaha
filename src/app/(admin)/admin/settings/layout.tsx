152c2e0f7076495c5831bb35e5e02b7dc4515adb
Dan Jordan <djordandrums@gmail.com>
2024-09-18 17:10:34 +0100

    add headings and links

--- src/app/(admin)/admin/settings/layout.tsx
+++ src/app/(admin)/admin/settings/layout.tsx
@@ -3,7 +3,9 @@ import React, { type ReactNode } from "react";
 const SettingsLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div>
-      <h2 className="pb-6 text-3xl font-bold">Settings</h2>
+      <a href="/admin/settings">
+        <h2 className="pb-6 text-3xl font-bold">Settings</h2>
+      </a>
       {children}
     </div>
   );
--- src/app/(admin)/admin/settings/openingHours/page.tsx
+++ src/app/(admin)/admin/settings/openingHours/page.tsx
@@ -2,19 +2,25 @@ import React from "react";
 import { days } from "~/server/types";
 import OpeningHoursCard from "./openingHoursCard";
 import { api } from "~/trpc/server";
+import BackButton from "../../tasters/@show/_backButton";
 
 const page = async () => {
   const result = await api.openingHours.list();
-  console.log("yooo", result);
   return (
-    <div className="flex flex-wrap gap-2">
-      {days.map((day) => (
-        <OpeningHoursCard
-          key={day}
-          day={day}
-          hours={result.find((r) => r.day === day)}
-        />
-      ))}
+    <div>
+      <span className="flex justify-between pb-2">
+        <h2 className="text-xl font-bold">Opening Times</h2>
+        <BackButton />
+      </span>
+      <div className="flex flex-wrap gap-2">
+        {days.map((day) => (
+          <OpeningHoursCard
+            key={day}
+            day={day}
+            hours={result.find((r) => r.day === day)}
+          />
+        ))}
+      </div>
     </div>
   );
 };
