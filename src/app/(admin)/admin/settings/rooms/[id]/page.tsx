e8c5337700a7b115a1a2ba9f14f1a4b0bb4b057e
Maria <ev.maria.pro@gmail.com>
2024-10-10 17:25:47 -0400

    UI for show room

--- src/app/(admin)/admin/settings/rooms/[id]/page.tsx
+++ src/app/(admin)/admin/settings/rooms/[id]/page.tsx
@@ -1,15 +1,40 @@
+"use client";
 import AdminButton from "~/app/_components/admin/Button";
+import BackButton from "../../../tasters/@show/_backButton";
 import React from "react";
 import Link from "next/link";
+import { api } from "~/trpc/react";
+import { useRouter } from "next/navigation";
 
-const page = () => {
+const ShowRoom = ({  params: { id }  }: { params: { id: string } }) => {
+    const { data, isLoading, isError, error } = api.rooms.show.useQuery({ id });
+    const router = useRouter();
+  
+    if (isLoading) {
+      return <div>Loading...</div>;
+    }
+    if (isError) return <p>{error.message}</p>;
     return (
-        <div><p>Hello</p>
-            <AdminButton>
-            <Link href="/admin/settings/rooms/update">Update this room</Link>
-            </AdminButton>
+        <div className="h-full w-full p-2">
+          <span className="flex justify-between">
+            <h3 className="text-xl font-bold">{data.name}</h3>
+            <span className="flex gap-2">
+              <AdminButton>
+                <Link href="/admin/settings/rooms/update">✏️ Edit</Link>
+              </AdminButton>
+          
+              <BackButton
+                returnPath="/admin/newClasses/list"
+                label="⬅️ Back to list"
+              />
+            </span>
+          </span>
+            <div>
+                <p>{data.description}</p>
+            </div>
+           
         </div>
     )  
 }
 
-export default page;
+export default ShowRoom;
